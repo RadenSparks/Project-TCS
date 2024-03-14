eb52414e9880973efea8d3eb06de9955a938723c
Phat <69975335+nhulieu@users.noreply.github.com>
2024-03-15 02:14:26 +0700

    update progresss

--- assets/php/addToCart.php
+++ assets/php/addToCart.php
@@ -5,7 +5,7 @@
     }
     $conn = mysqli_connect("localhost", "root", "", "db_epicgames") or die("Không thể kết nối!");
     mysqli_query($conn, "SET NAMES 'utf8'");
-    $check = mysqli_query($conn, "SELECT top 1 c.email, ci.gameid FROM cart c JOIN cartitem ci ON c.cartid = ci.cartid WHERE status = 1 and c.email = '".$_SESSION['email']."' and ci.gameid = '".$_GET['id']."'") or die(mysqli_error($conn));
+    $check = mysqli_query($conn, "SELECT c.email, ci.gameid FROM cart c JOIN cartitem ci ON c.cartid = ci.cartid WHERE status = 1 and c.email = '".$_SESSION['email']."' and ci.gameid = '".$_GET['id']."'") or die(mysqli_error($conn));
     if (mysqli_num_rows($check) > 0) {
         $cart_item_query = "INSERT INTO `cartitem`(`cartid`, `quantity`, `gameid`) VALUES ('".mysqli_insert_id($conn)."','1','".$_GET['id']."')";
         mysqli_close($conn);
--- views/pages/searchbar.php
+++ views/pages/searchbar.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col-sm-3 searchbar">
-      <form action="/Project-TCS/widget/search-page/searchpage.php" method="post" class="searchbox">
+      <form method="get" class="searchbox" id="searchbarform">
         <button style="background-color: #202020; border:none;" name="search" type="submit">
           <svg  width=" 20px" height="20px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" aria-labelledby="searchIconTitle" stroke="#fff" stroke-width="1" stroke-linecap="square" stroke-linejoin="miter" fill="none" color="#fff"> <title id="searchIconTitle">Search</title> <path d="M14.4121122,14.4121122 L20,20"/> <circle cx="10" cy="10" r="6"/> </svg>
         </button>
-        <input name="inputsearch" type="text"  placeholder="Search store">  
+        <input name="inputsearch" type="text" placeholder="Search store">  
       </form>
       <a href="index.php" class="searchbar-link">
         Discover
@@ -18,5 +18,11 @@
       <a href="index.php?site=cart" class="cart-link searchbar-link">Cart</a>
     </div>
 </div>
+<script>
+  document.getElementById("searchbarform").addEventListener("submit", function(event){
+    event.preventDefault()
+    window.location = "/Project-TCS/index.php?site=products&page=1&search=" + event.currentTarget.inputsearch.value;
+  });
+</script>
 
 
