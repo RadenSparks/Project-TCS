f7d6e79c2439e0f792f949c38c303464eaacc431
RadenSparks <89299148+RadenSparks@users.noreply.github.com>
2024-04-03 08:46:20 +0700

    update cart

--- static/php/createPayment.php
+++ static/php/createPayment.php
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 <?php
 session_start();
 if (!isset($_SESSION['email'])) {
-    header('Location: /Project-TCS/index.php?site=login');
+    header('Location: /Project-TCS/index.php?act=signin');
     exit;
 }
 
@@ -20,31 +19,11 @@ try {
 
     if ($check->rowCount() > 0) {
     } else {
-
-<?php    
-    session_start();
-    if(!isset($_SESSION['email'])){
-        header('Location: /Project-TCS/index.php?site=login');
-
     }
-    
-    $conn = mysqli_connect("localhost", "root", "", "db_epicgamers") or die("Không thể kết nối!");
-    mysqli_query($conn, "SET NAMES 'utf8'");
-    
-    if(mysqli_num_rows($check) > 0){
 
-
-    header('Location: /Project-TCS/index.php?site=home=' . $_GET['id']);
+    header('Location: /Project-TCS/index.php?act=home=' . $_GET['id']);
     exit;
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
-
-    }else{
-
-    }
-    
-    header('Location: /Project-TCS/index.php?site=home='.$_GET['id']);
-?>
-
