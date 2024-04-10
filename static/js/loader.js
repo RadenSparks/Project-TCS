697382f4a7dfba305df82c13c9f7bbc393f53455
RadenSparks <89299148+RadenSparks@users.noreply.github.com>
2024-04-10 10:42:58 +0700

    check

--- static/js/loader.js
+++
@@ -1,9 +0,0 @@
-let loading = document.querySelector(".preload")
-let content = document.getElementById("main")
-
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        loading.style.display = "none";
-        content.style.display = "block";
-    }, 1)
-})
--- static/php/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    echo "<script>window.location='/Project-TCS/index.php'</script>";
-?>
