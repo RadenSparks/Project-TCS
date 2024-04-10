9295d3368b1e3f0bf9567119393f35b892b46ece
Phat Luu Trong <phat.luu@cagemini.com>
2024-04-10 15:18:31 +0700

    Add validation on authen route

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ include_once "./static/model/product.php";
 include_once "./static/model/detail.php";
 include_once "./static/model/account.php";
 
-
-
-
 if (isset($_GET['act'])) {
     $act = $_GET['act'];
     switch ($act) {
@@ -44,8 +41,6 @@ if (isset($_GET['act'])) {
                 $sort = $_GET['sort'];
             }
 
-
-
             include_once "./static/php/header.php";
             include_once "./static/php/games.php";
             include_once "./static/php/footer.php";
@@ -62,51 +57,7 @@ if (isset($_GET['act'])) {
             include_once "./static/php/wishlist.php";
             include_once "./static/php/footer.php";
             break;
-
-        case 'addcart':
-            include_once "./static/php/header.php";
-
-            if (!isset($_SESSION['giohang'])) {
-                $_SESSION['giohang'] = [];
-                echo 'Lọt vào';
-            }
-            if (isset($_POST['submit']) && ($_POST['submit'])) {
-                $id = $_POST['id'];
-                $image = $_POST['image'];
-                $name = $_POST['name'];
-                $price = $_POST['price'];
-                $publisher = $_POST['publisher'];
-                $flag = 0;
-                echo '<h2 style="background: white; color: red;"  >' . $name . '</h2>';
-
-
-                if (isset($_SESSION['giohang']) && !empty($_SESSION['giohang'])) {
-                    echo '<h1 style="background: red; color: blue;">' . $price . '</h1>';
-                    $cart = $_SESSION['giohang'];
-                    echo var_dump($cart);
-                    foreach ($cart as $index => $item) {
-                        echo '<h1 style="background: green">' . $item[0] . '</h1>';
-                    }
-                } else {
-                    echo 'Connection failed';
-                }
-
-                if ($flag == 0) {
-                    $sp = array($id, $image, $name, $price, $publisher);
-                    $_SESSION['giohang'][] = $sp;
-                    echo 'Có giỏ hàng';
-                } else {
-                    echo ' KO Có giỏ hàng';
-                }
-            }
-            $cart = $_SESSION['giohang'];
-            echo var_dump($cart);
-
-            echo '<h1 style="background: white; color: black">Hello</h1>';
-            include_once "./static/php/cart.php";
-            include_once "./static/php/footer.php";
-
-            break;
+        
         case 'detail':
             include_once "./static/php/header.php";
 
@@ -117,50 +68,36 @@ if (isset($_GET['act'])) {
                 include_once "./static/php/detail.php";
             }
             include_once "./static/php/footer.php";
-
-            break;
-
-
-        case 'login':
-            include_once "./static/php/header.php";
-
-            // echo 'Hello';
-            $includeHeaderFooter = false;
-            $data = account();
-            if (isset($_POST['submit']) && $_POST['submit']) {
-
-                $email = $_POST['email'];
-                // echo '<h1>'.$email.'</h1>';
-                $password = $_POST['password'];
-                foreach ($data as $index => $item) {
-                    //    echo '<h1>'.$item['email'].'</h1>';
-                    if ($email == $item['email']) {
-                        if ($password == $item['password']) {
-                            include_once "./static/php/home.php";
-                        } else {
-                            include_once "./static/php/sign_in.php";
-                            $includeHeaderFooter = false;
-                        }
-                    }
-                }
-            }
-            include_once "./static/php/footer.php";
-
             break;
         case 'signin':
+            if (isset($_SESSION['email'])) {
+                header('Location: /Project-TCS/index.php');
+                exit;
+            }
             include_once "./static/php/sign_in.php";
             break;
         case 'register':
+            if (isset($_SESSION['email'])) {
+                header('Location: /Project-TCS/index.php');
+                exit;
+            }
             include_once "./static/php/register.php";
             break;
         case 'accountsetting':
+            if (!isset($_SESSION['email'])) {
+                header('Location: /Project-TCS/index.php');
+                exit;
+            }
             include_once "./static/php/header.php";
             echo '<script>const nav = document.querySelector(".sub-navbar_main_desktop__19-YT").style.display = "none"</script>';
             include_once "./static/php/accountAside.php";
             include_once "./static/php/accountsetting.php";
-            // include_once "./static/php/footer.php";
             break;
         case 'transaction':
+            if (!isset($_SESSION['email'])) {
+                header('Location: /Project-TCS/index.php');
+                exit;
+            }
             include_once "./static/php/header.php";
             echo '<script>const nav = document.querySelector(".sub-navbar_main_desktop__19-YT").style.display = "none"</script>';
             include_once "./static/php/accountAside.php";
